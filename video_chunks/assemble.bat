ebafaf70aa9176124b6416e9072e628da960abb5
rezasr098 <reza9sr18@gmail.com>
2026-05-13 20:06:34 +0330

    Delete video_chunks directory

--- video_chunks/assemble.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy /b video_part_* main_video.mp4
-echo ✅ Video reassembled as main_video.mp4
--- video_chunks/assemble.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cat video_part_* > main_video.mp4
-echo "✅ Video reassembled as main_video.mp4"
